f5f0ff42180cc8109109c3ea5ed6f601f8b995ed
gerryger <gabrielcaesario@gmail.com>
2016-02-26 19:02:07 +0700

    add admin page(front end only)

---
+++ admin/controller/login.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: user
+ * Date: 2016-02-26
+ * Time: 5:06 PM
+ */
+if($_POST && isset($_POST['username'], $_POST['password'])){
+
+}
---
+++ admin/views/adminHome.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--/**-->
+<!-- * Created by PhpStorm.-->
+<!-- * User: user-->
+<!-- * Date: 2016-02-26-->
+<!-- * Time: 5:21 PM-->
+<!-- */-->
+
+<head>
+    <meta charset="utf-8">
+    <title>.:M16 District:.</title>
+    <meta name="description" content="">
+    <!-- Mobile Specific Metas
+    ================================================== -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- load bootstrap css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+</head>
+<body>
+
+
+    <!-- Javascript files -->
+    <script type="text/javascript" src="js/bootstrap.js" />
+    <script type="text/javascript" src="js/bootstrap.min.js" />
+    <script type="text/javascript" src="js/jquery.js" />
+</body>
